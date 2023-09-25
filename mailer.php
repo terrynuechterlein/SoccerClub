46a034966a1ed487fad4db61fbd2560a13ed3497
terrynuechterlein <terrynuechterlein@gmail.com>
2023-09-25 07:09:09 -0400

    remox mailer.php comments

--- mailer.php
+++ mailer.php
@@ -8,13 +8,8 @@ require __DIR__ . "/vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-// print_r($_ENV);
 
 $mail = new PHPMailer(true);
-
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-// enables verbose debug output
-// $mail->SMTPDebug = 2; 
 
 $mail->isSMTP();
 $mail->SMTPAuth = true;
@@ -24,7 +19,6 @@ $mail->Host = $_ENV['MAIL_HOST'];
 $mail->Port = $_ENV['MAIL_PORT'];
 $mail->Username = $_ENV['MAIL_USERNAME'];
 $mail->Password = $_ENV['MAIL_PASSWORD'];
-
 
 $mail->isHtml(true);
 
